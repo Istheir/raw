96cd72a1f997c619e2a911b125424d568fad2c02
Istheir <54606972+Istheir@users.noreply.github.com>
2022-04-20 07:16:28 +0800

    Create newvps.sh

---
+++ newvps.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+export LC_ALL=C
+#export LANG=C
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
+
+sudoCmd=""
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+  sudoCmd="sudo"
+fi
+
+
+uninstall() {
+
+    ${sudoCmd} $(which rm) -rf $1
+
+    printf "File or Folder Deleted: %s\n" $1
+
+}
+
+# fonts color
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+blue(){
+    echo -e "\033[34m\033[01m$1\033[0m"
+}
+bold(){
+    echo -e "\033[1m\033[01m$1\033[0m"
+}
