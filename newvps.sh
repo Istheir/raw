f8d7bdf262a64056ce24c32c91937d3fcd374390
Istheir <54606972+Istheir@users.noreply.github.com>
2022-04-20 08:18:05 +0800

    Update newvps.sh

--- newvps.sh
+++ newvps.sh
@@ -25,6 +25,27 @@ bold(){
 
 mv /etc/localtime /etc/localtime.bak
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-apt install -y ntp
+
+
+apt install -y sudo
+apt install -y ntp  
+apt install -y nano 
+apt install -y vim wget git unzip zip tar curl
+
 systemctl enable ntp
 systemctl restart ntp
+
+apt install -y install cpu-checker
+apt install -y install -y xz-utils jq lsb-core lsb-release
+apt install -y install -y iputils-ping
+apt install -y install -y iperf3
+
+apt install -y gnupg2
+wget -O - https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
+
+  cat > "/etc/apt/sources.list.d/nginx.list" <<-EOF
+deb [arch=amd64] http://nginx.org/packages/debian/ $osReleaseVersionCodeName nginx
+deb-src http://nginx.org/packages/debian/ $osReleaseVersionCodeName nginx
+EOF
+update -y
+
