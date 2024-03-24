bd17ecac80305a4785bbb83619a38d466e69019e
Istheir <54606972+Istheir@users.noreply.github.com>
2024-03-24 22:10:42 +0800

    Update vlesssubwork.js

--- Rules/vlesssubwork.js
+++ Rules/vlesssubwork.js
@@ -5,8 +5,8 @@ let mytoken= ' ';//快速订阅访问入口, 留空则不启动快速订阅
 
 // 设置优选地址，不带端口号默认443，不支持非TLS订阅生成
 let addresses = [
-	'icook.tw:2053#优选域名',
-	'cloudflare.cfgo.cc#优选官方线路',
+	'icook.tw:2053',
+	'cloudflare.cfgo.cc',
 ];
 
 // 设置优选地址api接口
