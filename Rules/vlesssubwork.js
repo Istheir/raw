a93064e03d2a0ca3612323e18129c1598a483806
Istheir <54606972+Istheir@users.noreply.github.com>
2024-03-24 22:10:02 +0800

    Update vlesssubwork.js

--- Rules/vlesssubwork.js
+++ Rules/vlesssubwork.js
@@ -16,11 +16,11 @@ let addressesapi = [
 
 let DLS = 4;//速度下限
 let addressescsv = [
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressescsv.csv', //iptest测速结果文件。
+	//'https://raw.githubusercontent.com/Istheir/raw/master/Rules/addressescsv.csv', //iptest测速结果文件。
 ];
 
 let subconverter = "api.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅配置文件
+let subconfig = "https://github.com/Istheir/raw/raw/master/Rules/custom.ini"; //订阅配置文件
 let addressesnotls = [];
 let addressesnotlsapi = [
 	'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt',
